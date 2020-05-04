f4de0f2bf1c6df27a63a518103a4437d3bb4fbb9
Alberto Aragon <ronin8503@gmail.com>
2020-05-03 21:46:37 -0500

    using giphy api instead of pixabay, implementing load more feature

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,46 +1,72 @@
 import React, { useState, useEffect } from 'react';
 import ImageCard from './ImageCard';
 import ImageSearch from './ImageSearch';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   const [images, setImages] = useState([]);
-  const [isLoadind, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadingMore, setLoadingMore] = useState(false);
   const [term, setTerm] = useState('');
+  const [offset, setOffset] = useState(0);
+  // const apiURL = `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_APP_KEY}&q=${term}&image_type=photo&pretty=true`;
+  const apiURL = `https://api.giphy.com/v1/gifs/trending?api_key=${process.env.REACT_APP_GIPHY_APP_KEY}`;
 
   useEffect(() => {
-    fetch(
-      `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_APP_KEY}&q=${term}&image_type=photo&pretty=true`
-    )
+    fetch(`${apiURL}&offset=${offset}`)
       .then((res) => res.json())
-      .then((data) => {
-        setImages(data.hits);
+      .then((json) => {
+        setImages(json.data);
         setIsLoading(false);
+        console.log(json);
       })
       .catch((err) => console.log(err));
-  }, [term]);
+  }, [offset, apiURL]);
+
+  const loadMore = () => {
+    setLoadingMore(true);
+    fetch(`${apiURL}&offset=${images.length}`)
+      .then((res) => res.json())
+      .then((json) => {
+        setImages([...images, ...json.data]);
+        setIsLoading(false);
+        setLoadingMore(false);
+      })
+      .catch((err) => console.log(err));
+  };
 
   return (
     <div className='container mx-auto'>
-
       <ImageSearch searchText={(text) => setTerm(text)} />
 
-      {!isLoadind && images.length === 0 && (
+      {!isLoading && images.length === 0 && (
         <h1 className='text-6xl text-center mx-auto mt-32'>No images found</h1>
       )}
-      {isLoadind ? (
+      {isLoading ? (
         <h1 className='text-6xl text-center mx-auto mt-32'>Loading...</h1>
       ) : (
-          <div className='grid grid-cols-3 gap-4'>
-            {images.map((image, index) => (
-              <Link to={"/" + image.id}>
-                <ImageCard key={index} image={image} />
-              </Link>
-            ))}
-          </div>
-        )}
+        <div className='grid grid-cols-3 gap-4'>
+          {images.map((image, index) => (
+            <Link to={'/' + image.id} key={index}>
+              <ImageCard image={image} />
+            </Link>
+          ))}
+        </div>
+      )}
+      {loadingMore ? (
+        <h1 className='text-6xl text-center mx-auto mt-32'>Loading More...</h1>
+      ) : !isLoading ? (
+        <div className='max-w-sm rounder text-center overflow-hidden my-10 mx-auto'>
+          <button
+            className='bg-green-500 hover:bg-green-700 text-white font-bold py-4 px-8 rounded'
+            onClick={loadMore}
+          >
+            Load More
+          </button>
+        </div>
+      ) : null}
     </div>
   );
-}
+};
 
 export default Home;
